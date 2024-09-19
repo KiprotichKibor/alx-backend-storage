a762fd810648a53e29653b3b0b7bf57faf924b8e
KiprotichKibor <kiborotich24@gmail.com>
2024-09-19 14:38:49 +0000

    changes on syntax

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- Task: Create an index idx_name_first on the first letter of the name column in the names table
 -- This index will improve query performance for searches based on the first letter of names
 
-CREATE INDEX idx_name_first ON names (LEFT(NAME, 1));
+CREATE INDEX idx_name_first ON names(name(1));
